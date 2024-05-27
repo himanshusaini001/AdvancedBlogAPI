993b702f9eb4c16e2d3bf815be7434cc0ce5bd87
himanshu <himanshusaini26112002@gmail.com>
2024-05-27 11:46:26 +0530

    modified

---
+++ app/Console/Commands/RollbackCheck.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RollbackCheck extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:rollback-check';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
