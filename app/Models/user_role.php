07db3c59f1d03a071edf9e59c306b56f8d1c6568
himanshu <himanshusaini26112002@gmail.com>
2024-05-27 12:12:41 +0530

    modified

---
+++ app/Models/user_role.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+
+class user_role extends Model
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'role_id',
+    ];
+}
