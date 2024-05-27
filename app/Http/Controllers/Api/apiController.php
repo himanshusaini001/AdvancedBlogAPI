68bc778fd48d9a2bdc97aa1a79b5e4af14cadd73
himanshu <himanshusaini26112002@gmail.com>
2024-05-27 15:51:42 +0530

    modified

--- app/Http/Controllers/Api/apiController.php
+++ app/Http/Controllers/Api/apiController.php
@@ -6,10 +6,12 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class apiController extends Controller
 {
+    // Register function
     public function register(Request $request)
     {
         try{
@@ -34,7 +36,7 @@ class apiController extends Controller
         $users = User::create([
             'name'=>$request->name,
             'email'=>$request->email,
-            'password'=>$request->password,
+            'password' => Hash::make($request->password),
             'role_id'=>$request->role_id,
             
         ]);
@@ -50,9 +52,77 @@ class apiController extends Controller
         {
             return response()->json([
                 'status' => false,
-                'message' => 'An error occurred',
+                'message' => 'An error occurred Register',
                 'error' => $e->getMessage(),
             ]);
         }
     }
+
+    // User Login
+
+   public function login(Request $request)
+    {
+        try{
+
+            $validation = Validator::make($request->all(), [
+                'email'=> 'required|email',
+                'password'=> 'required',
+            ]);
+            if($validation->fails())
+            {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validation error',
+                    'errors' => $validation->errors()->all(),
+                ],401);
+            }
+            if(!Auth::attempt($request->only('email','password')))
+            {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Emial & password Dose not match With Out record.',
+                ],401);
+            }
+            $user = User::Where('email',$request->email)->first();
+            return response()->json([
+                'status' => true,
+                'message' => 'User login is successfully',
+                'token' => $user->createToken('Api Token')->plainTextToken,
+            ], 200);
+
+        }catch(\Exception $e)
+        {
+            return response()->json([
+                'ststus'=>false,
+                'message' => 'An error occurred in Login',
+                'error' => $e->getMessage(),
+            ]);
+        }
+    }
+
+    // User Profile
+
+    public function profile()
+    {
+       $user = auth()->user();
+       return response()->json([
+        'status'=>true,
+        'message'=>'Successfull Fetch data in Profile',
+        'data'=>$user
+       ]);
+
+    }
+
+     // User Logout
+     public function logout(Request $request)
+     {
+          // Get the authenticated user
+            $user = $request->user();
+            
+            // Revoke all tokens for the user
+            $user->tokens()->delete();
+            
+            return response()->json(['message' => 'Logged out successfully from all devices'], 200);
+     }
+     
 }
--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,27 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\apiController;
 
 // Register
-Route::post("user/register",[apiController::class,"register"]);
+Route::post("/user/register",[apiController::class,"register"]);
 
 // login
-Route::post("user/login",[apiController::class,"login"]);
+Route::post("/user/login",[apiController::class,"login"]);
+
+// user Route
+Route::group(['prefix' => 'user', 'middleware' => ['auth:sanctum']], function () {
+
+    // profile
+    Route::get("/profile",[apiController::class,"profile"]);   
+
+     // Logout
+     Route::delete("/logout",[apiController::class,"logout"]);   
+});
 
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::group(['prefix' => 'user', 'middleware' => ['auth:sanctum']], function () {
+
+    // profile
+    Route::get("/profile",[apiController::class,"profile"]);   
+
+     // Logout
+     Route::delete("/logout",[apiController::class,"logout"]);   
+});
