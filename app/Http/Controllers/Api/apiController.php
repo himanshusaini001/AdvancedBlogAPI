01114d4862d28077cc925b7eba17330133a99084
himanshu <himanshusaini26112002@gmail.com>
2024-05-27 13:01:18 +0530

    modified

--- app/Http/Controllers/Api/apiController.php
+++ app/Http/Controllers/Api/apiController.php
@@ -5,9 +5,19 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\ValidationData;
+use Illuminate\Support\Facades\Validator;
 
 class apiController extends Controller
 {
-   
+    public function register(Request $request)
+    {
+        try{
+
+        }catch(\Exception $e)
+        {
+            return response()->json([
+                
+            ]);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\apiController;
 
 // Register
-//Route::post("user/register",[ApiController::class,"register"]);
+Route::post("user/register",[apiController::class,"register"]);
+
+// login
+Route::post("user/login",[apiController::class,"login"]);
 
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
