f457b9d0e76061ea9f8eed3716ec0d38b4d7ace3
himanshu <himanshusaini26112002@gmail.com>
2024-05-27 12:31:31 +0530

    modified

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Register
+//Route::post("user/register",[ApiController::class,"register"]);
+
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
