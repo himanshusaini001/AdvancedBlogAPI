42f99051b04fa63d856184c89f4d4b9255d4676e
himanshu <himanshusaini26112002@gmail.com>
2024-05-27 11:26:50 +0530

    modified

--- app/Http/Controllers/Api/apiController.php
+++ app/Http/Controllers/Api/apiController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationData;
 
 class apiController extends Controller
 {
-    //
+   
 }
